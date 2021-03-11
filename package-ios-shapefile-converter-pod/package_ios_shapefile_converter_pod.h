4108ed37d63ca4f0ceb0c360507e98ffcfd89947
Luis Manuel Ramirez Vargas <luis_manuel.ramirez_vargas@syngenta.com>
2021-03-11 16:02:03 -0600

    Initial Commit

---
+++ package-ios-shapefile-converter-pod/package_ios_shapefile_converter_pod.h
@@ -0,0 +1,18 @@
+//
+//  package_ios_shapefile_converter_pod.h
+//  package-ios-shapefile-converter-pod
+//
+//  Created by Ramirez, Luis Manuel on 11/03/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for package_ios_shapefile_converter_pod.
+FOUNDATION_EXPORT double package_ios_shapefile_converter_podVersionNumber;
+
+//! Project version string for package_ios_shapefile_converter_pod.
+FOUNDATION_EXPORT const unsigned char package_ios_shapefile_converter_podVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <package_ios_shapefile_converter_pod/PublicHeader.h>
+
+
---
+++ package-ios-shapefile-converter-podTests/package_ios_shapefile_converter_podTests.swift
@@ -0,0 +1,33 @@
+//
+//  package_ios_shapefile_converter_podTests.swift
+//  package-ios-shapefile-converter-podTests
+//
+//  Created by Ramirez, Luis Manuel on 11/03/21.
+//
+
+import XCTest
+@testable import package_ios_shapefile_converter_pod
+
+class package_ios_shapefile_converter_podTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
